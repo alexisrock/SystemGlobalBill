6afe9c95eb0b9d2d87f34965ff5a47104ce7ef19
alexis bueno <alexisrock20@gmail.com>
2016-12-07 10:11:04 -0500

    login con filtro 10:10 am

--- src/java/com/globalbill/frontend/controller/Managed/LoginContoller.java
+++ src/java/com/globalbill/frontend/controller/Managed/LoginContoller.java
@@ -14,67 +14,62 @@ import javax.inject.Named;
 import java.io.Serializable;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
-import javax.ejb.Stateless;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
-import javax.faces.bean.ManagedBean;
-
-
 import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-
 
 /**
  *
  * @author Administrador
  */
 //@Named(value = "loginContoller")
-@Named(value="loginContoller")
+@Named(value = "loginContoller")
 @SessionScoped
 public class LoginContoller implements Serializable {
- 
-        
+
     @EJB
-    private  UsuarioasignadoFacadeLocal usuarioF;
-    
+    private UsuarioasignadoFacadeLocal usuarioF;
     private Usuarioasignado usuarioa;
-        
-    public LoginContoller() {
-  
+
+    @PostConstruct
+
+    public void init() {
+
+        usuarioa = new Usuarioasignado();
+
     }
-    
-    
-   public String validarUsuario(){
-       String url = null;
+
+    public LoginContoller() {
+
+    }
+
+    public String validarUsuario() {
+        String url = null;
         Usuarioasignado usuariovalido;
-        usuariovalido = usuarioF.iniciosession(this.usuarioa);
-     try{
-       if (usuariovalido != null){
-           HttpSession sesion = (HttpSession)FacesContext.getCurrentInstance().getExternalContext().getSession(true);
-             sesion.setAttribute("usuarioLogin", usuariovalido);             
-           url = "/protegido/index.xhtml?faces-redirect=true";
-           System.out.println("despues del url");
-     }else{
-           System.out.println("hago esto");
-            FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_ERROR, 
-            "usuario y/o la contraseña no son validos", "");
-            FacesContext.getCurrentInstance().addMessage(null, fm);
-       
-     }
-     }catch(Exception e){
-          FacesContext.getCurrentInstance().addMessage(
-                    null,new FacesMessage(FacesMessage.SEVERITY_FATAL,"Error: ", "datos incorrectos"));
         
-     
-     }
-       System.out.println("linea final");
-return url;
-  }
-           
-  
-   
-   
+        try {
+            usuariovalido = usuarioF.iniciosession(this.usuarioa);
+            if (usuariovalido != null) {
+                HttpSession sesion = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+                sesion.setAttribute("usuarioLogin", usuariovalido);
+                url = "/protegido/index.xhtml?faces-redirect=true";
+                System.out.println("despues del url");
+            } else {
+                System.out.println("hago esto");
+                FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                        "usuario y/o la contraseña no son validos", "");
+                FacesContext.getCurrentInstance().addMessage(null, fm);
+
+            }
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(
+                    null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Error: ", "datos incorrectos"));
+
+        }
+        System.out.println("linea final");
+        return url;
+    }
 
     public Usuarioasignado getUsuarioa() {
         return usuarioa;
@@ -83,15 +78,5 @@ return url;
     public void setUsuarioa(Usuarioasignado usuarioa) {
         this.usuarioa = usuarioa;
     }
-    
-    
-    @PostConstruct
-    
-public void init(){
 
-     usuarioa= new Usuarioasignado();
-     
- 
-}
-    
 }
